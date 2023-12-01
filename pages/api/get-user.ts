ac863ccb247e06e064f2bdc3aad4345f3d0ab0aa
Deeksha Rungta <deeksharu1902@gmail.com>
2023-12-02 03:31:54 +0530

    fix: enhance error handling in get-user api

--- pages/api/get-user.ts
+++ pages/api/get-user.ts
@@ -10,28 +10,35 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const cookies = parse(req.headers.cookie || "");
-  const uuidValue = cookies.auth;
-  const sessionToken = await redis.get(uuidValue);
-  const jwtSecret = process.env.NEXT_PUBLIC_JWT_SECRET || "sdwfesbydtnfmg";
+  try {
+    const cookies = parse(req.headers.cookie || "");
+    const uuidValue = cookies.auth;
+    const sessionToken = await redis.get(uuidValue);
+    const jwtSecret = process.env.NEXT_PUBLIC_JWT_SECRET || "sdwfesbydtnfmg";
 
-  if (!sessionToken) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+    if (!sessionToken) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
 
-  const email = jwt.verify(sessionToken, jwtSecret) as {
-    email: {
-      email: string;
+    const decodedToken = jwt.verify(sessionToken, jwtSecret) as {
+      email: { email: string };
     };
-  };
 
-  const user = await prisma.user.findUnique({
-    where: { email: email.email.email },
-  });
+    if (!decodedToken || !decodedToken.email || !decodedToken.email.email) {
+      return res.status(401).json({ error: "Invalid token format" });
+    }
 
-  if (user === null) {
-    return res.status(404).json({ error: "User not found" });
+    const user = await prisma.user.findUnique({
+      where: { email: decodedToken.email.email },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    return res.status(200).json({ email: user.email });
+  } catch (error) {
+    console.error("Error in API handler:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-
-  res.status(200).json({ email: user.email });
 }
